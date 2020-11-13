fc469474f0d5b75b94aaa0813165c33571b8093b
Fafhner <Fafhner@users.noreply.github.com>
2020-11-13 12:25:40 +0100

    cw5

--- ZTPD/cw IV/cw_sdo.sql
+++ ZTPD/cw IV/cw_sdo.sql
@@ -6,12 +6,10 @@ KSZTALT MDSYS.SDO_GEOMETRY
 INSERT INTO FIGURY VALUES (1, MDSYS.SDO_GEOMETRY(
             2003, NULL, NULL,
             MDSYS.SDO_ELEM_INFO_ARRAY(
-                1, 1003, 4
+                1,1003,4
             ),
             MDSYS.SDO_ORDINATE_ARRAY(
-                5, 3,
-                7, 5,
-                5, 7
+                5,3, 7,5, 5,7
             ))
 );
 
@@ -19,11 +17,10 @@ INSERT INTO FIGURY VALUES (1, MDSYS.SDO_GEOMETRY(
 INSERT INTO FIGURY VALUES (2, MDSYS.SDO_GEOMETRY(
             2003, NULL, NULL,
             MDSYS.SDO_ELEM_INFO_ARRAY(
-                1, 1003, 3
+                1,1003,3
             ),
             MDSYS.SDO_ORDINATE_ARRAY(
-                1, 1,
-                5, 5
+                1,1, 5,5
             ))
 );
 
@@ -31,20 +28,27 @@ INSERT INTO FIGURY VALUES (2, MDSYS.SDO_GEOMETRY(
 INSERT INTO FIGURY VALUES (3, MDSYS.SDO_GEOMETRY(
             2006, NULL, NULL,
             MDSYS.SDO_ELEM_INFO_ARRAY(
-                1, 4, 2,
-                1, 2, 1,
-                3, 2, 2
+                1,4,2, 1,2,1, 5,2,2
             ),
             MDSYS.SDO_ORDINATE_ARRAY(
-                3, 2,
-                6, 2,
-                7, 3,
-                8, 2,
-                7, 3
+                3,2, 6,2, 
+                7,3, 8,2, 7,1
             ))
 );
 
-INSERT INTO FIGURY VALUES(4, MDSYS.SDO_GEOMETRY(2003,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,1),MDSYS.SDO_ORDINATE_ARRAY(5,1,8,1,8,6,5,7,5,1)));
+--- Nieprawidlowy ---
+INSERT INTO FIGURY VALUES (4, MDSYS.SDO_GEOMETRY(
+            2003, NULL, NULL,
+            MDSYS.SDO_ELEM_INFO_ARRAY(
+                1,1003,3
+            ),
+            MDSYS.SDO_ORDINATE_ARRAY(
+                1,1, 5,5, 5,4
+            ))
+);
 
 select SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(ksztalt, 0.1) from figury;
+
+delete from FIGURY where id=4;
 commit;
+
---
+++ ZTPD/ćw V/cw_1_przetwarzanie_danych_przestrzennych.sql
@@ -0,0 +1,18 @@
+INSERT INTO user_sdo_geom_metadata VALUES (
+    'FIGURY',
+    'KSZTALT',
+    mdsys.sdo_dim_array(
+        mdsys.sdo_dim_element('X', 0, 9, 0.01), 
+        mdsys.sdo_dim_element('Y', 0, 9, 0.01)),
+    NULL
+);
+
+select SDO_TUNE.ESTIMATE_RTREE_INDEX_SIZE(3000000,8192,10,2,0) from dual;
+
+create index FIGURY_KSZTALT_IDX on FIGURY(KSZTALT)INDEXTYPE IS MDSYS.SPATIAL_INDEX_V2;
+
+select ID from FIGURY 
+where SDO_FILTER(KSZTALT, SDO_GEOMETRY(2001, null, SDO_POINT_TYPE(3,3,null),null,null)) = 'TRUE';
+
+select ID from FIGURY 
+where SDO_RELATE(KSZTALT,SDO_GEOMETRY(2001,null,SDO_POINT_TYPE(3,3,null),null,null),'mask=ANYINTERACT') = 'TRUE';
