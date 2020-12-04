ddf32735278502a47a6152bc9a0058b8591ba1c9
Fafhner <Fafhner@users.noreply.github.com>
2020-12-04 13:23:07 +0100

    spatial-lrs

---
+++ ZTPD/ćw VII/spatial-lrs.sql
@@ -0,0 +1,70 @@
+/*****
+
+Æwiczenie 1
+
+*****/
+
+
+CREATE TABLE A6_LRS (
+    GEOM SDO_GEOMETRY
+);
+
+
+insert into A6_LRS
+select SR.GEOM
+from   STREETS_AND_RAILROADS SR, MAJOR_CITIES C
+where  SDO_RELATE(SR.GEOM, SDO_GEOM.SDO_BUFFER(C.GEOM, 10, 1, 'unit=km'),'MASK=ANYINTERACT') = 'TRUE'
+and    C.CITY_NAME = 'Koszalin';
+
+
+select SDO_GEOM.SDO_LENGTH(SR.GEOM, 1, 'unit=km') DISTANCE, ST_LINESTRING(SR.GEOM).ST_NUMPOINTS() ST_NUMPOINTS
+from   A6_LRS SR;
+
+
+UPDATE a6_lrs 
+SET a6_lrs.geom = SDO_LRS.CONVERT_TO_LRS_GEOM(a6_lrs.GEOM, 0, 276.681);
+
+
+INSERT INTO USER_SDO_GEOM_METADATA
+VALUES ('A6_LRS', 'GEOM', 
+    MDSYS.SDO_DIM_ARRAY(MDSYS.SDO_DIM_ELEMENT('X', 12.603676, 26.369824, 1),
+                        MDSYS.SDO_DIM_ELEMENT('Y', 45.8464, 58.0213, 1),
+                        MDSYS.SDO_DIM_ELEMENT('M', 0, 300, 1)),8307);
+            
+            
+CREATE INDEX a6_lrs_idx ON A6_LRS(geom) 
+    INDEXTYPE IS MDSYS.SPATIAL_INDEX;
+                    
+
+/*****
+
+Æwiczenie 2
+
+*****/
+
+
+select SDO_LRS.VALID_MEASURE(GEOM, 500) VALID_500
+from   A6_LRS;
+
+
+select SDO_LRS.GEOM_SEGMENT_END_PT(GEOM) END_PT 
+from A6_LRS;
+
+
+select SDO_LRS.LOCATE_PT(GEOM, 150, 0) KM150 from A6_LRS;
+
+
+select SDO_LRS.CLIP_GEOM_SEGMENT(GEOM, 120, 160) CLIPED_120_160 from A6_LRS;
+
+
+select SDO_LRS.GET_NEXT_SHAPE_PT(A6.GEOM, SDO_LRS.PROJECT_PT(A6.GEOM, C.GEOM)) WJAZD_NA_A6
+from   A6_LRS A6, MAJOR_CITIES C
+where  C.CITY_NAME = 'Slupsk';
+
+
+select SDO_LRS.GEOM_SEGMENT_LENGTH(
+        SDO_LRS.OFFSET_GEOM_SEGMENT(A6.GEOM, M.DIMINFO, 50, 200, 10, 'unit=m')
+        )/1000 KOSZT
+from   A6_LRS A6, USER_SDO_GEOM_METADATA M
+where  M.TABLE_NAME = 'A6_LRS'
+and    M.COLUMN_NAME = 'GEOM';
